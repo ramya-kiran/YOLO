2368a3a238b5de7c8f5eb756996933547c3f6eb8
Ramya Rao <ramrao@umail.iu.edu>
2017-11-10 15:00:10 -0500

    initial commit

---
+++ util.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+ALPHA = 0.1
+
+
+def convolution_layer(input_imgs, outputs, size, stride, name):
+    with tf.name_scope('conv_' + name):
+        channels = input_imgs.get_shape()[3]
+        weights = tf.get_variable("w_"+name, [size, size, channels, outputs], initializer=tf.contrib.layers.xavier_initialization())
+        biases = tf.get_variable("b_"+name, [outputs], initializer=tf.constant_initializer(0.0))
+        tf.summary.histogram('conv_w_'+name, weights)
+        tf.summary.histogram('conv_b_'+name, biases)
+        conv = tf.nn.conv2d(input_imgs, weights, strides[1,stride, stride, 1], padding='SAME', name='conv_w_'+name)
+        conv_biased = tf.add(conv, biases, name='conv_b'+name)
+        
+        return tf.maximum(ALPHA*conv_biased, conv_biased, name='leaky_relu_'+name)
+
+
+def pooling_layer(input_tensor, pool_size, pool_stride, name):
+    with tf.name_scope('pool_'+name):
+        return tf.nn.max_pool(input_tensor, ksize=[1,pool_size, pool_size, 1], strides=[1,pool_stride, pool_stride,1], padding='SAME', name = 'pool_'+name)
+
+
+def fc_layer(input_tensor, no_out, no_in, leaky):
+    with tf.name_scope('fc_'+name):
+        weights = tf.get_variable("fc_w_"+name, [no_in, no_out], initializer=tf.contrib.layers.xavier_initialization())
+        biases = tf.get_variable("fc_b_"+name, [no_out], initializer=tf.constant_initializer(0.0))
+        y = tf.add(tf.matmul(input_tensor, weights), biases)
+        tf.summary.histogram('fc_w'+name, weights)
+        tf.summary.histogram('fc_b'+name, biases)
+        
+        return(tf.maximum(ALPHA*y, y, name='fc_out'+name))
---
+++ yolo_model.py
@@ -0,0 +1,44 @@
+import util.py
+import tensorflow as tf
+
+
+def model(input_tensor):
+    # [448x448x3] -->  [224x224x16]
+    conv_1_out = convolution_layer(input_tensor, 16, 3, 1, '1')
+    pool_1_out = pooling_layer(conv_1_out, 2, 2, '1')
+    
+    # [224x224x16] --> [112x112x32]
+    conv_2_out = convolution_layer(pool_1_out, 32, 16, 1, '2')
+    pool_2_out = pooling_layer(conv_2_out, 2, 2, '2')
+    
+    # [112x112x32] --> [56x56x64]
+    conv_3_out = convolution_layer(pool_2_out, 64, 32, 1, '3')
+    pool_3_out = pooling_layer(conv_3_out, 2, 2, '3')
+    
+    # [56x56x64] --> [28x28x128]
+    conv_4_out = convolution_layer(pool_3_out, 128, 64, 1, '4')
+    pool_4_out = pooling_layer(conv_4_out, 2, 2, '4')
+
+    # [28x28x128] --> [14x14x256]
+    conv_5_out = convolution_layer(pool_4_out, 256, 128, 1, '5')
+    pool_5_out = pooling_layer(conv_5_out, 2, 2, '5')
+    
+    # [14x14x256] --> [7x7x512]
+    conv_6_out = convolution_layer(pool_5_out, 512, 256, 1, '6')
+    pool_6_out = pooling_layer(conv_6_out, 2, 2, '6')
+    
+    # [7x7x512] --> [7x7x1024]
+    conv_7_out = convolution_layer(pool_6_out, 1024, 512, 1, '7')
+    
+    # [7x7x1024] --> [7x7x1024]
+    conv_8_out = convolution_layer(conv_7_out, 1024, 1024, 1, '8')
+    
+    # output = [batch_size, 7*7*1024]
+    flatten_1 = tf.reshape(conv_8_out, [-1, 7*7*1024])
+    
+    fc_1_out = fc_layer(flatten_1, 256, 7*7*1024, True)
+    fc_2_out = fc_layer(fc_1_out, 256, 256, True)
+    fc_3_out = fc_layer(fc_2_out, 256, 4096, True)
+
+    return fc_3_out
+    
