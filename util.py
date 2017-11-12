ea6912f454d8ad4e8f4ec56a431def23094a433a
Ramya Rao <ramrao@umail.iu.edu>
2017-11-12 12:40:41 -0500

    training code added

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-import global_declare.py
-ALPHA = 0.1
+from global_declare import *
 
 
+# convolutional layer to be used by the model 
 def convolution_layer(input_imgs, outputs, size, stride, name):
     with tf.name_scope('conv_' + name):
         channels = input_imgs.get_shape()[3]
@@ -16,11 +16,13 @@ def convolution_layer(input_imgs, outputs, size, stride, name):
         return tf.maximum(ALPHA*conv_biased, conv_biased, name='leaky_relu_'+name)
 
 
+# pooling layer
 def pooling_layer(input_tensor, pool_size, pool_stride, name):
     with tf.name_scope('pool_'+name):
         return tf.nn.max_pool(input_tensor, ksize=[1,pool_size, pool_size, 1], strides=[1,pool_stride, pool_stride,1], padding='SAME', name = 'pool_'+name)
 
 
+# fully connected layer
 def fc_layer(input_tensor, no_out, no_in, leaky):
     with tf.name_scope('fc_'+name):
         weights = tf.get_variable("fc_w_"+name, [no_in, no_out], initializer=tf.contrib.layers.xavier_initialization())
@@ -30,3 +32,35 @@ def fc_layer(input_tensor, no_out, no_in, leaky):
         tf.summary.histogram('fc_b'+name, biases)
         
         return(tf.maximum(ALPHA*y, y, name='fc_out'+name))
+
+
+# loss layer
+def loss_layer(pred, actual, batch_size):
+    with tf.name_Scope('loss_layer'):
+        val_1 = GRID_SIZE * GRID_SIZE * NO_CLASSES
+        val_2 = val_1 + (GRID_SIZE * GRID_SIZE * NO_BOUNDING_BOX)
+        pred_classes = np.reshape(pred[:, : val_1], [batch_size, GRID_SIZE, GRID_SIZE, NO_CLASSES])
+        pred_scales = np.reshape(pred[:, val_1: val_2], [batch_size, GRID_SIZE, GRID_SIZE, NO_BOUNDING_BOX])
+        pred_boxes = np.reshape(pred[:, val_2:], [batch_size, GRID_SIZE, GRID_SIZE, NO_BOUNDING_BOX, 4])
+        
+        response = tf.reshape(actual[:,:,:,0], [batch_size, GRID_SIZE, GRID_SIZE, NO_BOUNDING_BOX, 1])
+        boxes = tf.reshape(actual[:,:,:,1:5], [batch_size,GRID_SIZE, GRID_SIZE, 1, 4])
+        boxes = tf.tile(boxes, [1,1,1, NO_BOUNDING_BOX, 1]) / IMAGE_SIZE
+        classes = actual[:,:,:,5:]
+
+        offset = np.transpose(np.reshape(np.array(
+            [np.arange(GRID_SIZE)] * GRID_SIZE * NO_BOUNDING_BOX),
+            (NO_BOUNDING_BOX, GRID_SIZE, GRID_SIZE)), (1, 2, 0))
+        
+        offset = tf.constant(offset, dtype=tf.float32)
+        offset = tf.reshape(offset, [1, GRID_SIZE, GRID_SIZE, NO_BOUNDING_BOX])
+        offset = tf.tile(offset, [batch_size, 1, 1, 1])
+        predict_boxes_tran = tf.stack([(pred_boxes[:, :, :, :, 0] + offset) / GRID_SIZE,
+                (pred_boxes[:, :, :, :, 1] + tf.transpose(offset, (0, 2, 1, 3))) / GRID_SIZE,
+                tf.square(pred_boxes[:, :, :, :, 2]),
+                tf.square(pred_boxes[:, :, :, :, 3])])
+        predict_boxes_tran = tf.transpose(predict_boxes_tran, [1, 2, 3, 4, 0])
+
+        iou_predict_truth = cal_iou(predict_box_tran,boxes)
+
+        
---
+++ yolo_train.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import argparse 
+from util import *
+from global_declare import *
+from yolo_model import *
+import time
+
+if __name__ == '__main__':
+    # Read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('train', nargs='+', help='tf record filename')
+    parser.add_argument('-b', '--batch-size', default=10, type=int, help='batch size')
+    parser.add_argument('-e', '--epochs', default=4000, type=int, help='num of epochs')
+    parser.add_argument('-o', '--output', default='model', help='model output')
+    parser.add_argument('-l', '--log', default='logs', help='log directory')
+    args = parser.parse_args()
+
+    # Reading the tf files and obtaining images and labels
+    filename_queue = tf.train.string_input_producer(args.train)
+    image, label = read_and_decode(filename_queue)
+    batch = tf.train.shuffle_batch([image, label], batch_size=args.batch_size, capacity=800, num_threads=2, min_after_dequeue=200)
+    
+    # placeholders for input and labels
+    X = tf.placeholder(tf.float32, [None, IMAGE_SIZE IMAGE_SIZE, CHANNEL], name='X')
+    y = tf.placeholder(tf.float32, [None, GRID_SIZE, GRID_SIZE, 5+NO_CLASSES], name='labels')
+    
+    # passing the parameters to modela and getting output
+    y_hat = model(X)
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        
+        merged_summary = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(args.log, sess.graph)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        
+        for i in range(args.epochs):
+            images, labels = sess.run(batch)
+            sess.run(train, feed_dict={X: images, y: labels})
+            
+            # Print training accuracy every 100 epochs
+            if (i+1) % 100 == 0:
+                print('loss val {}: {:.2f}'.format(i+1, sess.run(loss_val, feed_dict={X1: images1, X2:images2, y: labels1})))
+                
+            if (i+1) % 1000 == 0:
+                params = saver.save(sess, '{}_{}.ckpt'.format(args.output, i+1))
+                print('Model saved: {}'.format(params))
+                
+        coord.request_stop()
+        coord.join(threads)
+    
+    
